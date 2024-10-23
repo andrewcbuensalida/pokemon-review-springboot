54cf18b6769a4fedbbe5b31423e4240a3631c130
Andrew Buensalida <andrewcbuensalida@gmail.com>
2024-10-22 19:15:57 -0700

    prettier

--- src/main/java/com/pokemonreview/api/controllers/AuthController.java
+++ src/main/java/com/pokemonreview/api/controllers/AuthController.java
@@ -27,48 +27,48 @@ import java.util.Collections;
 @RequestMapping("/api/auth")
 public class AuthController {
 
-    private AuthenticationManager authenticationManager;
-    private UserRepository userRepository;
-    private RoleRepository roleRepository;
-    private PasswordEncoder passwordEncoder;
-    private JWTGenerator jwtGenerator;
+  private AuthenticationManager authenticationManager;
+  private UserRepository userRepository;
+  private RoleRepository roleRepository;
+  private PasswordEncoder passwordEncoder;
+  private JWTGenerator jwtGenerator;
 
-    @Autowired
-    public AuthController(AuthenticationManager authenticationManager, UserRepository userRepository,
-                          RoleRepository roleRepository, PasswordEncoder passwordEncoder, JWTGenerator jwtGenerator) {
-        this.authenticationManager = authenticationManager;
-        this.userRepository = userRepository;
-        this.roleRepository = roleRepository;
-        this.passwordEncoder = passwordEncoder;
-        this.jwtGenerator = jwtGenerator;
+  @Autowired
+  public AuthController(AuthenticationManager authenticationManager, UserRepository userRepository,
+      RoleRepository roleRepository, PasswordEncoder passwordEncoder, JWTGenerator jwtGenerator) {
+    this.authenticationManager = authenticationManager;
+    this.userRepository = userRepository;
+    this.roleRepository = roleRepository;
+    this.passwordEncoder = passwordEncoder;
+    this.jwtGenerator = jwtGenerator;
+  }
+
+  @PostMapping("login")
+  public ResponseEntity<AuthResponseDTO> login(@RequestBody LoginDto loginDto) {
+    Authentication authentication = authenticationManager.authenticate(
+        new UsernamePasswordAuthenticationToken(
+            loginDto.getUsername(),
+            loginDto.getPassword()));
+    SecurityContextHolder.getContext().setAuthentication(authentication);
+    String token = jwtGenerator.generateToken(authentication);
+    return new ResponseEntity<>(new AuthResponseDTO(token), HttpStatus.OK);
+  }
+
+  @PostMapping("register")
+  public ResponseEntity<String> register(@RequestBody RegisterDto registerDto) {
+    if (userRepository.existsByUsername(registerDto.getUsername())) {
+      return new ResponseEntity<>("Username is taken!", HttpStatus.BAD_REQUEST);
     }
 
-    @PostMapping("login")
-    public ResponseEntity<AuthResponseDTO> login(@RequestBody LoginDto loginDto){
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                loginDto.getUsername(),
-                loginDto.getPassword()));
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        String token = jwtGenerator.generateToken(authentication);
-        return new ResponseEntity<>(new AuthResponseDTO(token), HttpStatus.OK);
-    }
+    UserEntity user = new UserEntity();
+    user.setUsername(registerDto.getUsername());
+    user.setPassword(passwordEncoder.encode((registerDto.getPassword())));
 
-    @PostMapping("register")
-    public ResponseEntity<String> register(@RequestBody RegisterDto registerDto) {
-        if (userRepository.existsByUsername(registerDto.getUsername())) {
-            return new ResponseEntity<>("Username is taken!", HttpStatus.BAD_REQUEST);
-        }
+    Role roles = roleRepository.findByName("USER").get();
+    user.setRoles(Collections.singletonList(roles));
 
-        UserEntity user = new UserEntity();
-        user.setUsername(registerDto.getUsername());
-        user.setPassword(passwordEncoder.encode((registerDto.getPassword())));
+    userRepository.save(user);
 
-        Role roles = roleRepository.findByName("USER").get();
-        user.setRoles(Collections.singletonList(roles));
-
-        userRepository.save(user);
-
-        return new ResponseEntity<>("User registered success!", HttpStatus.OK);
-    }
+    return new ResponseEntity<>("User registered success!", HttpStatus.OK);
+  }
 }
--- src/main/java/com/pokemonreview/api/controllers/ReviewController.java
+++ src/main/java/com/pokemonreview/api/controllers/ReviewController.java
@@ -13,39 +13,43 @@ import java.util.List;
 @RequestMapping("/api/")
 public class ReviewController {
 
-    private ReviewService reviewService;
+  private ReviewService reviewService;
 
-    @Autowired
-    public ReviewController(ReviewService reviewService) {
-        this.reviewService = reviewService;
-    }
+  @Autowired
+  public ReviewController(ReviewService reviewService) {
+    this.reviewService = reviewService;
+  }
 
-    @PostMapping("/pokemon/{pokemonId}/reviews")
-    public ResponseEntity<ReviewDto> createReview(@PathVariable(value = "pokemonId") int pokemonId, @RequestBody ReviewDto reviewDto) {
-        return new ResponseEntity<>(reviewService.createReview(pokemonId, reviewDto), HttpStatus.CREATED);
-    }
+  @PostMapping("/pokemon/{pokemonId}/reviews")
+  public ResponseEntity<ReviewDto> createReview(@PathVariable(value = "pokemonId") int pokemonId,
+      @RequestBody ReviewDto reviewDto) {
+    return new ResponseEntity<>(reviewService.createReview(pokemonId, reviewDto), HttpStatus.CREATED);
+  }
 
-    @GetMapping("/pokemon/{pokemonId}/reviews")
-    public List<ReviewDto> getReviewsByPokemonId(@PathVariable(value = "pokemonId") int pokemonId) {
-        return reviewService.getReviewsByPokemonId(pokemonId);
-    }
+  @GetMapping("/pokemon/{pokemonId}/reviews")
+  public List<ReviewDto> getReviewsByPokemonId(@PathVariable(value = "pokemonId") int pokemonId) {
+    return reviewService.getReviewsByPokemonId(pokemonId);
+  }
 
-    @GetMapping("/pokemon/{pokemonId}/reviews/{id}")
-    public ResponseEntity<ReviewDto> getReviewById(@PathVariable(value = "pokemonId") int pokemonId, @PathVariable(value = "id") int reviewId) {
-        ReviewDto reviewDto = reviewService.getReviewById(pokemonId, reviewId);
-        return new ResponseEntity<>(reviewDto, HttpStatus.OK);
-    }
+  @GetMapping("/pokemon/{pokemonId}/reviews/{id}")
+  public ResponseEntity<ReviewDto> getReviewById(@PathVariable(value = "pokemonId") int pokemonId,
+      @PathVariable(value = "id") int reviewId) {
+    ReviewDto reviewDto = reviewService.getReviewById(pokemonId, reviewId);
+    return new ResponseEntity<>(reviewDto, HttpStatus.OK);
+  }
 
-    @PutMapping("/pokemon/{pokemonId}/reviews/{id}")
-    public ResponseEntity<ReviewDto> updateReview(@PathVariable(value = "pokemonId") int pokemonId, @PathVariable(value = "id") int reviewId,
-                                                  @RequestBody ReviewDto reviewDto) {
-        ReviewDto updatedReview = reviewService.updateReview(pokemonId, reviewId, reviewDto);
-        return new ResponseEntity<>(updatedReview, HttpStatus.OK);
-    }
+  @PutMapping("/pokemon/{pokemonId}/reviews/{id}")
+  public ResponseEntity<ReviewDto> updateReview(@PathVariable(value = "pokemonId") int pokemonId,
+      @PathVariable(value = "id") int reviewId,
+      @RequestBody ReviewDto reviewDto) {
+    ReviewDto updatedReview = reviewService.updateReview(pokemonId, reviewId, reviewDto);
+    return new ResponseEntity<>(updatedReview, HttpStatus.OK);
+  }
 
-    @DeleteMapping("/pokemon/{pokemonId}/reviews/{id}")
-    public ResponseEntity<String> deleteReview(@PathVariable(value = "pokemonId") int pokemonId, @PathVariable(value = "id") int reviewId) {
-        reviewService.deleteReview(pokemonId, reviewId);
-        return new ResponseEntity<>("Review deleted successfully", HttpStatus.OK);
-    }
+  @DeleteMapping("/pokemon/{pokemonId}/reviews/{id}")
+  public ResponseEntity<String> deleteReview(@PathVariable(value = "pokemonId") int pokemonId,
+      @PathVariable(value = "id") int reviewId) {
+    reviewService.deleteReview(pokemonId, reviewId);
+    return new ResponseEntity<>("Review deleted successfully", HttpStatus.OK);
+  }
 }
